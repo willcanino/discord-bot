91695240ec6262296820e80c7c19aa6873b1c235
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-17 22:21:21 -0800

    Added basic leveling system

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,26 @@
 import discord
 from discord.ext import commands
 from bot.tables import Base as base, UserXP
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 class bot(commands.Bot):
 	def __init__(self, *args, uri='sqlite:///:memory:', **kwargs):
 		self.engine = create_async_engine(uri)
+		self.session = AsyncSession(bind=self.engine)
 		super().__init__(*args, **kwargs)
 
 	async def on_connect(self):
 		async with self.engine.begin() as conn:
 			await conn.run_sync(base.metadata.create_all)
+
+	async def on_message(self, message):
+		if not message.author.bot:
+			def add_user(session):
+				user = session.query(UserXP).filter(UserXP.discord_id == message.author.id) \
+					   .one_or_none()
+				if user is None:
+					user = UserXP(discord_id=message.author.id, xp=0)
+					session.add(user)
+				user.xp += 1
+			await self.session.run_sync(add_user)
+			await self.session.commit()
