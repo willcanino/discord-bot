a1501585cc59765a7b5988ae8c19d7358c4bfe75
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-17 23:15:03 -0800

    Added get_or_create method used for creating a user if they do not exist (when attempting to fetch a user)

--- bot/tables.py
+++ bot/tables.py
@@ -9,5 +9,14 @@ class UserXP(Base):
 	discord_id = Column(BigInteger, primary_key=True)
 	xp = Column(Integer, nullable=False)
 
+	@classmethod
+	def get_or_create(cls, session, discord_id):
+		user = session.query(cls).filter(cls.discord_id == discord_id) \
+			   .one_or_none()
+		if user is None:
+			user = cls(discord_id=discord_id, xp=0)
+			session.add(user)
+		return user
+
 	def __repr__(self):
 		return f'<UserXP(discord_id={self.discord_id}, xp={self.xp})>'
