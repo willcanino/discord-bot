be7f64d99c6ffd83301389e082c839bae0da36e3
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-17 21:43:08 -0800

    Configure SQL database when creating instance of the bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,13 @@
 import discord
 from discord.ext import commands
+from bot.tables import Base as base, UserXP
+from sqlalchemy.ext.asyncio import create_async_engine
 
 class bot(commands.Bot):
-	pass
+	def __init__(self, *args, uri='sqlite:///:memory:', **kwargs):
+		self.engine = create_async_engine(uri)
+		super().__init__(*args, **kwargs)
+
+	async def on_connect(self):
+		async with self.engine.begin() as conn:
+			await conn.run_sync(base.metadata.create_all)
