1273c80c17f1fefed486422566654c6ffb9b1177
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-26 21:38:02 -0800

    Added guild_id Column to UserXP table
    (and updated __repr__ to reflect said changes)

--- bot/tables.py
+++ bot/tables.py
@@ -7,6 +7,7 @@ class UserXP(Base):
 	__tablename__ = 'userxp'
 
 	discord_id = Column(BigInteger, primary_key=True)
+	guild_id = Column(BigInteger, primary_key=True)
 	xp = Column(Integer, nullable=False)
 
 	@classmethod
@@ -19,4 +20,4 @@ class UserXP(Base):
 		return user
 
 	def __repr__(self):
-		return f'<UserXP(discord_id={self.discord_id}, xp={self.xp})>'
+		return f'<UserXP(discord_id={self.discord_id}, guild_id={self.guild_id}, xp={self.xp})>'
