6992f69faf91e5d9ad2b0f93dc52ccf42f64d901
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-26 21:03:55 -0800

    Deleted JavaScript code from Github

--- commands/youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'youtube',
-    description: "sends the youtube channel link:",
-    execute(message, args) {
-        message.channel.send('<https://www.youtube.com/willcanino>');
-    }
-}
