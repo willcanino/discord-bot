5574705e7bd75ba6d07c033adc5a779791bbecb8
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-26 23:42:27 -0800

    Added level property to UserXP class

--- bot/tables.py
+++ bot/tables.py
@@ -1,14 +1,28 @@
+import itertools
 from sqlalchemy import Column, PrimaryKeyConstraint, Integer, BigInteger
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 class UserXP(Base):
+    LEVEL_ONE_AMOUNT = 100
+    INCREASE_PER_LEVEL = 50
+
     __tablename__ = 'userxp'
 
     discord_id = Column(BigInteger, primary_key=True)
     guild_id = Column(BigInteger, primary_key=True)
     xp = Column(Integer, default=0, nullable=False)
+
+    @property
+    def level(self):
+        xp = self.xp
+        counter = itertools.count(start=1)
+        while xp >= 0:
+            level = next(counter)
+            xp -= self.LEVEL_ONE_AMOUNT + self.INCREASE_PER_LEVEL * (level - 1)
+        return level
+
 
     @classmethod
     def get_or_create(cls, session, discord_id, guild_id, commit=False):
